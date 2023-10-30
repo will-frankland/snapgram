1e5d7a309bf4fa296a28aa4b9251c218c6253573
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-30 13:37:49 -0400

    setup auth and root folders asnd signin / signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './globals.css';
+import { Home } from './_root/pages';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Snapgram!
-    </h1>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route path='/sign-in' element={<SigninForm />} />
+        <Route path='/sign-up' element={<SignupForm />} />
+
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   )
 }
 
