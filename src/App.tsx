7720f5a72f9d67836699edae511326a46949531f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-30 13:40:12 -0400

    setup paths for public and private

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import './globals.css';
-import { Home } from './_root/pages';
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
+import "./globals.css";
+import { Home } from "./_root/pages";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <main className='flex h-screen'>
+    <main className="flex h-screen">
       <Routes>
         {/* public routes */}
-        <Route path='/sign-in' element={<SigninForm />} />
-        <Route path='/sign-up' element={<SignupForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
         {/* private routes */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
