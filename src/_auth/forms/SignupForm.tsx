25c68b40c73e74a44f2f99dff2589c1327b179b1
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-11-01 11:22:52 -0400

    import shadcn btn

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
 
 const SignupForm = () => {
   return (
     <div>
-      SignupForm
+      <Button>Click Me</Button>
     </div>
   )
 }
