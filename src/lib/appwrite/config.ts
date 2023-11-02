1ba53c2ffe470de8d80d0af3d260fbdd290405ce
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-11-02 10:54:52 -0400

    setup API file with createNewUser function and add types Interfaces

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,5 +1,4 @@
 import { Client, Account, Databases, Storage, Avatars } from 'appwrite';
-import { Database } from 'lucide-react';
 
 export const appwriteConfig = {
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
