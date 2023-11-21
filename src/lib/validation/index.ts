070f09e8b72455fe7913c8f35d132167f46eec0d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-11-21 17:42:37 -0500

    duplicate signup into signin page and modify to suit - tested working

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -6,3 +6,8 @@ export const signupValidation = z.object({
   email: z.string().email(),
   password: z.string().min(8, { message: "Password must be at least 8 characters" })
 });
+
+export const signinValidation = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Password must be at least 8 characters" })
+});
