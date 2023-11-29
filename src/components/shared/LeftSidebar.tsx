bab5e17e590f592a698e215e1515dbb270f922ff
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-11-28 19:52:33 -0500

    add group tag to li tag to fix invert white error not showing

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -42,7 +42,7 @@ const LeftSidebar = () => {
           {sidebarLinks.map((link: INavLink) => {
             const isActive = pathname === link.route;
             return (
-              <li key={link.label} className={`leftsidebar-link ${isActive && 'bg-primary-500'}`}>
+              <li key={link.label} className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`}>
                 <NavLink
                   to={link.route}
                   className="flex gap-4 items-center p-4"
