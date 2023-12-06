450c757f19363d2ab2f2c36054a792d8140f7f94
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-12-06 15:47:44 -0500

    create validator for posts and import to PostForm

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -11,3 +11,11 @@ export const signinValidation = z.object({
   email: z.string().email(),
   password: z.string().min(8, { message: "Password must be at least 8 characters" })
 });
+
+export const postValidation = z.object({
+  caption: z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(1000),
+  tags: z.string()
+  
+});
