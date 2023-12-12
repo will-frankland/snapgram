47a46462bb4d5f32c6aa0b0dccaa10cefe32c0ab
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-12-12 16:59:46 -0500

    create logic to handle likes and post saves - TESTED

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -13,6 +13,7 @@ import {
   likePost,
   savePost,
   deleteSavedPost,
+  getCurrentUser,
 } from "../appwrite/api";
 import { INewPost, INewUser } from "@/types";
 import { QUERY_KEYS } from "./queryKeys";
@@ -125,3 +126,10 @@ export const useDeleteSavedPost = () => {
     }
   });
 };
+
+export const useGetCurrentUser = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+    queryFn: getCurrentUser
+  })
+}
